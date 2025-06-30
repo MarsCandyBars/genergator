f240f134f7964cc502c2a171f35b37d4fd655817
MarsCandyBars <63179967+MarsCandyBars@users.noreply.github.com>
2025-06-29 19:50:08 -0500

    Update readme and adjust input sanitization

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Genergator
 
-A password generator app
+![Screenshot](src/assets/pictures/genergator.png)
+
+
+## Description
+
+A password generator app. 
+
+## Installation
+# Web
+```sh
+1. Clone the repo
+2. cd into the directory
+3. Run npm install
+4. Run ng serve -o
+```
+# Electron
+```sh
+1. Clone the repo
+2. cd into the directory
+3. Run npm install
+4. Run npm run electron
+```
--- src/app/main-generator/main-generator.component.ts
+++ src/app/main-generator/main-generator.component.ts
@@ -22,26 +22,28 @@ export class MainGeneratorComponent {
     this.mustInclude = this.mustInclude;
     this.result = '';
 
+    const lengthValidity = typeof this.length === "number" || this.length === null;
+    const mustIncludeValidity = this.mustInclude.length === 1 || this.mustInclude.length === 0;
+
     if (this.mustInclude.length > 1 || this.mustInclude < 0) {
-      alert("You may only include a single character in the input field");
+      alert("You may only include a single character in the Must Include field");
       throw new Error("More than a single character was input into the Must Include field");
     } else if (typeof this.length != "number" || null) {
       alert("You may only input numbers into the Length field");
       throw new Error("A non-numeric value was input into the Length field")
-    }
-
-    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+    } else if (lengthValidity && mustIncludeValidity) {
+      let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
     
-    let charactersLength = characters.length;
-    for (let i = 0; i < this.length; i++) {
-      this.result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
+      let charactersLength = characters.length;
+      for (let i = 0; i < this.length; i++) {
+        this.result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
 
-    if (this.mustInclude) {
-      let resultArray = this.result.split('');
-      resultArray[Math.floor(Math.random() * this.length)] = this.mustInclude;
-      this.result = resultArray.join("");
-    }
+      if (this.mustInclude) {
+        let resultArray = this.result.split('');
+        resultArray[Math.floor(Math.random() * this.length)] = this.mustInclude;
+        this.result = resultArray.join("");
+      }
+    } 
   }
-
 }
